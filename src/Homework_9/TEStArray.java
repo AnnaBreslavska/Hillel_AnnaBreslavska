e31253c6c68547f57eb3f536f281d797b92df274
AnnaBreslavska <anna.hepalova@itechcraft.com>
2021-06-07 11:05:39 +0300

    7th HW!!

---
+++ src/Homework_9/TEStArray.java
@@ -0,0 +1,17 @@
+package Homework_9;
+
+import java.util.ArrayList;
+
+public class TEStArray {
+    public static void main(String[] args) {
+        ArrayList<Double> arrayListSize = new ArrayList<>(5);
+        arrayListSize.add(1.1);
+        arrayListSize.add(2.2);
+        arrayListSize.add(3.3);
+        arrayListSize.add(4.4);
+        arrayListSize.add(5.5);
+        arrayListSize.add(6.6);
+        arrayListSize.add(7.7);
+        System.out.println(arrayListSize);
+    }
+}
--- src/Homework_9/Task1.java
+++ src/Homework_9/Task1.java
@@ -1,28 +1,27 @@
 package Homework_9;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-
         System.out.println("You are going to sum 5 values!");
-        getSum();
+        System.out.println("The Sum of entered values is: " + getSum());
     }
 
     public static double getSum() {
         Scanner scanner = new Scanner(System.in);
-        double[] values = new double[5];
+        ArrayList<Double> arrayListSize = new ArrayList<>();
         double sum = 0;
         for (int i = 1; i < 6; i++) {
             int currentNumber = i;
             System.out.println(" Enter " + currentNumber + " value:");
             double number = scanner.nextDouble();
-            for (double n : values) {
+            for (double n : arrayListSize) {
                 n = number;
-                sum = sum + number;
             }
+            sum = sum + number;
+            arrayListSize.add(number);
         }
         return sum;
     }
